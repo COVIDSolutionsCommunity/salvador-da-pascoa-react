1a9e2c8c144f95e1769d46f8f74fc38ae5681a06
Marina Tavares <marinastavares6@gmail.com>
2020-04-03 15:42:45 -0300

    feature: add material ui

---
+++ src/app/index.js
@@ -0,0 +1,285 @@
+import React, { useEffect, useState, useCallback, useMemo } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Tabletop from 'tabletop'
+import MenuItem from '@material-ui/core/MenuItem'
+import Select from '@material-ui/core/Select'
+import FormControl from '@material-ui/core/FormControl'
+import InputLabel from '@material-ui/core/InputLabel'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+
+import useStyles from './styles'
+
+const BRAZILIAN_STATES = [
+  'AC',
+  'AL',
+  'AM',
+  'AP',
+  'BA',
+  'CE',
+  'DF',
+  'ES',
+  'GO',
+  'MA',
+  'MT',
+  'MS',
+  'MG',
+  'PA',
+  'PB',
+  'PR',
+  'PE',
+  'PI',
+  'RJ',
+  'RN',
+  'RO',
+  'RS',
+  'RR',
+  'SC',
+  'SE',
+  'SP',
+  'TO',
+]
+
+const INITIAL_STATE = [
+  {
+    ddata: '03/04/2020 12:28:27',
+    name: 'marirlandia',
+    email: 'marinastavares6@gmail.com',
+    state: 'SC',
+    city: 'Florianópolis',
+    neighborhood: 'cebtor',
+    delivery: 'Telefone, Uber Eats',
+    'phoneNumberr ': '+5548984140707',
+    whatsapp: '5548984140707',
+    site: 'chocolate.com',
+    instagram: '@chocolate',
+    ifood: '',
+    uberEats: '',
+    rappi: '',
+    howToReceive: 'Delivery',
+    photo: 'https://drive.google.com/uc?id=18ADNmJyMZARQhJZbFwSxHa9vIW7XIp9s',
+    obs: 'Trabalho fazem anos com delivery',
+  },
+  {
+    ddata: '03/04/2020 12:28:27',
+    name: 'marirlandia',
+    email: 'marinastavares6@gmail.com',
+    state: 'SC',
+    city: 'Florianópolis',
+    neighborhood: 'cebtor',
+    delivery: 'Telefone, Uber Eats',
+    'phoneNumberr ': '+5548984140707',
+    whatsapp: '5548984140707',
+    site: 'chocolate.com',
+    instagram: '@chocolate',
+    ifood: '',
+    uberEats: '',
+    rappi: '',
+    howToReceive: 'Delivery',
+    photo: 'https://drive.google.com/uc?id=18ADNmJyMZARQhJZbFwSxHa9vIW7XIp9s',
+    obs: 'Trabalho fazem anos com delivery',
+  },
+  {
+    ddata: '03/04/2020 12:28:27',
+    name: 'marirlandia',
+    email: 'marinastavares6@gmail.com',
+    state: 'SC',
+    city: 'Florianópolis',
+    neighborhood: 'cebtor',
+    delivery: 'Telefone, Uber Eats',
+    phoneNumber: '+5548984140707',
+    whatsapp: '5548984140707',
+    site: 'chocolate.com',
+    instagram: '@chocolate',
+    ifood: '',
+    uberEats: '',
+    rappi: '',
+    howToReceive: 'Delivery',
+    photo: 'https://drive.google.com/uc?id=18ADNmJyMZARQhJZbFwSxHa9vIW7XIp9s',
+    obs: 'Trabalho fazem anos com delivery',
+  },
+  {
+    ddata: '03/04/2020 12:28:27',
+    name: 'marirlandia',
+    email: 'marinastavares6@gmail.com',
+    state: 'SC',
+    city: 'Florianópolis',
+    neighborhood: 'cebtor',
+    delivery: 'Telefone, Uber Eats',
+    'phoneNumberr ': '+5548984140707',
+    whatsapp: '5548984140707',
+    site: 'chocolate.com',
+    instagram: '@chocolate',
+    ifood: '',
+    uberEats: '',
+    rappi: '',
+    howToReceive: 'Delivery',
+    photo: 'https://drive.google.com/uc?id=18ADNmJyMZARQhJZbFwSxHa9vIW7XIp9s',
+    obs: 'Trabalho fazem anos com delivery',
+  },
+  {
+    ddata: '03/04/2020 12:28:27',
+    name: 'marirlandia',
+    email: 'marinastavares6@gmail.com',
+    state: 'SC',
+    city: 'Florianópolis',
+    neighborhood: 'cebtor',
+    delivery: 'Telefone, Uber Eats',
+    'phoneNumberr ': '+5548984140707',
+    whatsapp: '5548984140707',
+    site: 'chocolate.com',
+    instagram: '@chocolate',
+    ifood: '',
+    uberEats: '',
+    rappi: '',
+    howToReceive: 'Delivery',
+    photo: 'https://drive.google.com/uc?id=18ADNmJyMZARQhJZbFwSxHa9vIW7XIp9s',
+    obs: 'Trabalho fazem anos com delivery',
+  },
+  {
+    ddata: '03/04/2020 12:28:27',
+    name: 'marirlandia',
+    email: 'marinastavares6@gmail.com',
+    state: 'SC',
+    city: 'Chapecó',
+    neighborhood: 'cebtor',
+    delivery: 'Telefone, Uber Eats',
+    'phoneNumberr ': '+5548984140707',
+    whatsapp: '5548984140707',
+    site: 'chocolate.com',
+    instagram: '@chocolate',
+    ifood: '',
+    uberEats: '',
+    rappi: '',
+    howToReceive: 'Delivery',
+    photo: 'https://drive.google.com/uc?id=18ADNmJyMZARQhJZbFwSxHa9vIW7XIp9s',
+    obs: 'Trabalho fazem anos com delivery',
+  },
+]
+
+// mudar o open para up da photo
+
+const App = () => {
+  const styles = useStyles()
+  const [age, setAge] = useState('SC')
+  const [city, setCity] = useState('')
+  const [clients, setClients] = useState(INITIAL_STATE)
+
+  const handleChange = useCallback((event) => {
+    setAge(event.target.value)
+  }, [])
+
+  const handleChange2 = useCallback((event) => {
+    setCity(event.target.value)
+  }, [])
+
+  const stateCities = useMemo(() => clients.find((client) => client.state === age), [
+    age,
+    clients,
+  ])
+  console.log('App -> stateCities', stateCities)
+
+  // useEffect(() => {
+  //   Tabletop.init({
+  //     key: '1CYuCMQ35yXrPX6Pjjq_7CS6AXOjAN-2BbjMq8uWcP04',
+  //     callback: (data, tabletop) => {
+  //       console.log('App -> data', data)
+  //       setClients(data)
+  //     },
+  //     simpleSheet: true,
+  //   })
+  // }, [])
+
+  return (
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      className={styles.container}
+    >
+      <Typography
+        className={styles.title}
+        component="h1"
+        color="primary"
+        variant="h2"
+      >
+        Salvador da Páscoa
+      </Typography>
+      <Grid container className={styles.select} justify="center" alignItems="center">
+        <FormControl className={styles.formControl}>
+          <InputLabel htmlFor="age-native-simple">Estado</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            onChange={handleChange}
+          >
+            {BRAZILIAN_STATES.map((state) => (
+              <MenuItem value={state}>{state}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl className={styles.formControl}>
+          <InputLabel htmlFor="age-native-simple">Cidade</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={city}
+            onChange={handleChange2}
+          >
+            {clients.map(
+              (client) =>
+                client.state === age && (
+                  <MenuItem key={client.name} value={client.city}>
+                    {client.city}
+                  </MenuItem>
+                )
+            )}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid className={styles.cards}>
+        {clients.map(
+          (client) =>
+            client.city === city && (
+              <Card key={client.name} className={styles.root}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="140"
+                    image={client.photo}
+                    title="Contemplative Reptile"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {client.name}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      {client.obs}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Entrar em contato
+                  </Button>
+                  <Button size="small" color="primary">
+                    Saber mais
+                  </Button>
+                </CardActions>
+              </Card>
+            )
+        )}
+      </Grid>
+    </Grid>
+  )
+}
+
+export default React.memo(App)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,130 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './app'
+import * as serviceWorker from './serviceWorker'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#EFCEC5',
+    },
+    secondary: {
+      main: '#fff',
+    },
+    custom: {
+      warmGrey: '#909090',
+      greyishBrown: '#525252',
+      fadedGreen: '#72b26c',
+      dustyTeal: '#4d878e',
+      midBlue2: '#2e64aa',
+      warmGrey2: '#8f8f8f',
+      wildSand: '#f5f5f5',
+      cobalt: '#204d99',
+      darkIndo: '#0f2450',
+      dustyGrey: '#969696',
+      fern: '#72B26C',
+      smalt: '#4D878E',
+      azure: '#2E64AA',
+      fadedRed: '#dd485b',
+      paleGrey: '#ecf3f5',
+      greyish: '#b8b8b8',
+      midBlue: '#265bb2',
+      red: '#ff0000',
+    },
+    background: {
+      default: 'white',
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: '4.8px',
+      fontWeight: 'bold',
+    },
+    h2: {
+      fontSize: '20px',
+      fontWeight: 'bold',
+    },
+    h3: {
+      fontSize: '16px',
+      fontWeight: 'bold',
+      lineHeight: '1.5',
+    },
+    h4: {
+      fontSize: '14px',
+      fontWeight: 'bold',
+    },
+    h5: {
+      fontSize: '12px',
+      fontWeight: 'bold',
+      lineHeight: '1.33',
+    },
+    subtitle1: {
+      fontSize: '16px',
+      lineHeight: '1.5',
+    },
+  },
+  root: {
+    backgroundColor: 'white',
+    button: {
+      textTransform: 'capitalize',
+    },
+    input: {
+      fontSize: '16px',
+      appearance: 'none',
+    },
+  },
+  overrides: {
+    MuiDialogTitle: {
+      root: {
+        fontSize: '2px',
+      },
+    },
+    MuiSvgIcon: {
+      root: {
+        height: '20px',
+        width: '20px',
+      },
+    },
+    MuiTypography: {
+      body1: {
+        fontSize: '16px',
+        lineHeight: '1.5',
+      },
+    },
+    MuiFormLabel: {
+      root: {
+        fontSize: '16px',
+      },
+    },
+    MuiInputBase: {
+      input: {
+        fontSize: '16px',
+        lineHeight: 'normal',
+      },
+    },
+    MuiButton: {
+      containedPrimary: {
+        color: 'white',
+      },
+    },
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
