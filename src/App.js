7502284b2e6b287cdbe8e1ffb55f129e1249aaa5
Marina Tavares <marinastavares6@gmail.com>
2020-04-03 22:44:57 -0300

    feat: add loading state

--- src/App.js
+++ src/App.js
@@ -8,16 +8,12 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site em construção
+          <br />
+          <a class="App-link" href="https://instagram.com/salvadordapascoa">
+            @salvadoradapascoa
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   )
